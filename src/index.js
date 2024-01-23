69e36c0dc1bc87372960dc29d1ae20466c5e36e7
David <david01ms@outlook.com>
2024-01-23 09:37:20 +0100

    Initial commit: A demo Node.js application.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello World! Listening on port ${port}.</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}.`);
+});
